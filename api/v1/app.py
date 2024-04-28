a0815a95cf18d010fe6479b8a81e35dee2e5b6f0
Tashydaije <mbuguanatasha01@gmail.com>
2024-04-28 04:24:21 +0300

    fix blueprint registration err

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from api.v1.views import app_views
+from api.v1.views import app_views, register_routes
+from api.v1 import db
 
-# 3. Configure your Flask app
+# Configure your Flask app
 app = Flask(__name__)
 app.config.from_object('config.Config')
+db.init_app(app)
+# Call register_routes to define routes before registering blueprint
+register_routes(app_views, app)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
-# 4. Initialize SQLAlchemy
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-# 6. Run the Flask app
+# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True, port=5000)  # Set debug=True for development environment
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,20 @@
 #API blueprint initialization
-
 from flask import Blueprint
+from api.v1 import db
+
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
+def register_routes(bp, app):
+  #Register routes defined in this directory.
+  from . import index  
+  from . import users   
+
+  # Register routes from imported files
+  bp.route("/status")(index.api_status) 
+
+  # Register users routes
+  bp.route('/users', methods=['POST'])(users.create_user)
+  bp.route('/users', methods=['GET'])(users.get_users)
+  bp.route('/users/<id>', methods=['GET'])(users.get_user)
+  bp.route('/users/<id>', methods=['PUT'])(users.update_user)
+  bp.route('/users/<id>', methods=['DELETE'])(users.delete_user)
